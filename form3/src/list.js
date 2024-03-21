7af2dba517204468f4e4c4700535e3f863bfdaa1
trannhutphat23 <trannhutphattv@gmail.com>
2024-03-22 00:59:30 +0700

    submit 1,3,4

---
+++ form3/src/list.js
@@ -0,0 +1,11 @@
+const list = [
+    ["nt", "Biển Nha Trang", "./assets/bien-nha-trang.jpg"],
+    ["dl", "Thành phố Đà Lạt", "./assets/thanh-pho-da-lat.jpg"],
+    ["vt", "Biển Vũng Tàu", "./assets/bien-vung-tau.jpg"],
+    ["hl", "Vịnh Hạ Long", "./assets/vinh-ha-long.jpg"],
+    ["pt", "Biển Phan Thiết", "./assets/bien-phan-thiet.png"],
+    ["ht", "Biển Hà Tiên", "./assets/bien-ha-tien.jpg"],
+    ["pq", "Đảo Phú Quốc", "./assets/dao-phu-quoc.jpg"],
+]
+
+export default list
