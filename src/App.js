c0c18a7f883fe80aba221dd2848cabe923431071
ThuHuong03 <ntthuong010603@gmail.com>
2024-03-23 23:46:27 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./Style.css";
 import Form3 from "./Component/Form3";
 import React, { useState } from "react";
 import Form4 from "./Component/Form4";
+import Form1 from "./Component/Form1";
 function App() {
   return(
       <div className="App">
  <Form3/>
+ {/* Nếu muốn hiển thị bài khác, đổi Form3 thành Form1 hoặc Form4 */}
   </div>
 
   )
